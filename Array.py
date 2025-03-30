1760dbc5b5c8f471ede20f8806f6471657d9eb34
Kartikay Rane <86407649+18kdr@users.noreply.github.com>
2025-03-30 22:33:57 +0530

    Array Operations and Time complexity

--- Array.py
+++ Array.py
@@ -1,14 +1,64 @@
-# There are two types in which we can work with arrays in python
+"""
+Some basic regarding time complexity while performing array operations
 
-# List in theory
-# -> The most common way to use a array is this in python
-# -> We can store hetrogenious elements in the list. (Ex: We can store string, int and float in the list unlike the array)
+Inserting the element at the end -> O(1)
+Inserting the element at a pos -> O(n)
+Deleting the element at the end -> O(1)
+Deleting the element at a pos -> O(n)
+Searching for element with index given -> O(1)
+Searching for the element throughout -> O(n)
+Modifying one element -> O(1)
+Modifying all the elements -> O(n)
+
+"""
+
+#List is used for hetrogenous storage whereas array is used for homogenous storage
+
 # List in code
 listNew = [1,2,3,4,5,6]
+
+# Insert in list
+listNew[0] = 10
+
+#Search in the list
+
+# -- To search an element is in the list or not
+if 5 in listNew:
+    print("True")
+    
+# -- To search an element if it is not in the list
+if 100 not in listNew:
+    print("True")
+
+# -- To search an element with using a for loop
+for i in range(0,len(listNew)):
+    if i == 5:
+        print("Element found")
+
+# -- To search an element while using for loop
+k = len(listNew)
+while k > 0:
+    if k == 6:
+        print("found")
+    k -= 1 
+
+# Delete in list
+del(listNew[-1])
+
+# Modify elements
+
+# -- Modifying one element
+listNew[-1] = 100
+
+# -- Modifying multiple element [Sqaure each item]
+listSquare = []
+for i in listNew:
+    listSquare.append(i*i)
+    
+print(listSquare)
+
 print(listNew)
-
 # Array in code
-
 import array as arr
 
 arrNew = arr.array('i',listNew)
